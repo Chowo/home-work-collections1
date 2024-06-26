4ce01434eadd940411fcfece0c5f512295f1b81b
Chowo <bob-xiii@rambler.ru>
2024-06-26 06:55:20 +0200

    Task solved

--- src/main/java/pro/sky/home_work_collections1/service/EmployeeAlreadyAddedException.java
+++ src/main/java/pro/sky/home_work_collections1/service/EmployeeAlreadyAddedException.java
@@ -1,5 +1,9 @@
 package pro.sky.home_work_collections1.service;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus (value = HttpStatus.ALREADY_REPORTED, reason = "Добавляемый работник уже есть в списке")
 public class EmployeeAlreadyAddedException extends RuntimeException {
     public EmployeeAlreadyAddedException() {
         super("Добавляемый работник уже есть в списке");
--- src/main/java/pro/sky/home_work_collections1/service/EmployeeNotFoundException.java
+++ src/main/java/pro/sky/home_work_collections1/service/EmployeeNotFoundException.java
@@ -1,5 +1,9 @@
 package pro.sky.home_work_collections1.service;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Работник не найден")
 public class EmployeeNotFoundException extends RuntimeException{
     public EmployeeNotFoundException() {
         super("Работник не найден");
