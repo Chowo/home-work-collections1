ce54d1344a94015f8cc13688169d7895b9dde054
Chowo <bob-xiii@rambler.ru>
2024-07-03 19:17:58 +0200

    Array to Map changed

--- src/main/java/pro/sky/home_work_collections1/model/Employee.java
+++ src/main/java/pro/sky/home_work_collections1/model/Employee.java
@@ -19,6 +19,10 @@ public class Employee {
         return lastName;
     }
 
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
     public boolean equals(Object object) {
         if (this == object) {
             return true;
--- src/main/java/pro/sky/home_work_collections1/service/EmployeeService.java
+++ src/main/java/pro/sky/home_work_collections1/service/EmployeeService.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 @Service
 public class EmployeeService implements EmployeeServiceInterface {
-    private Map<String, Employee> employees;
+    private final Map<String, Employee> employees;
     private static final int EMPLOYEE_LIMIT = 5;
 
     public EmployeeService() {
@@ -26,27 +26,24 @@ public class EmployeeService implements EmployeeServiceInterface {
             throw new EmployeeStorageIsFullException();
         }
         Employee newEmployee = new Employee(firstName, lastName);
-        if (employees.containsValue(newEmployee)) {
+        if (employees.containsKey(newEmployee.getFullName())) {
             throw new EmployeeAlreadyAddedException();
         }
-        String key = firstName + " " + lastName;
-        employees.put(key, newEmployee);
+        employees.put(newEmployee.getFullName(), newEmployee);
         return newEmployee;
     }
 
     public Employee deleteEmployee(String firstName, String lastName) throws EmployeeNotFoundException {
         Employee deletableEmployee = new Employee(firstName, lastName);
-        if (!employees.containsValue(deletableEmployee)) {
+        if (!employees.containsKey(deletableEmployee.getFullName())) {
             throw new EmployeeNotFoundException();
         }
-        String key = firstName + " " + lastName;
-        employees.remove(key);
-        return deletableEmployee;
+        return employees.remove(deletableEmployee.getFullName());
     }
 
     public Employee findEmployee(String firstName, String lastName) throws EmployeeNotFoundException {
         Employee goalEmployee = new Employee(firstName, lastName);
-        if (employees.containsValue(goalEmployee)) {
+        if (employees.containsKey(goalEmployee.getFullName())) {
             return goalEmployee;
         } else throw new EmployeeNotFoundException();
     }
